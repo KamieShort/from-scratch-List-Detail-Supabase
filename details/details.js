fc1e1f17a1d42d176b1d338f69ddc620cd88c338
Kamie Short <kamie.short@gmail.com>
2022-02-11 18:06:51 -0800

    changed to include correct function

--- details/details.js
+++ details/details.js
@@ -1,43 +1,43 @@
 // import { fetchMember } from '../fetch-utils.js';
 import { fetchMember } from '../fetch-utils.js';
-// import { renderMemberDetails } from '../render-utils.js';
+import { renderMemberDetails } from '../render-utils.js';
 
-// const memberList = document.getElementById('member-detail-container');
+const memberList = document.getElementById('member-detail-container');
 
 // // eslint-disable-next-line space-before-function-paren
 // // window.addEventListener('load', async () => {
 // //     console.log(member);
 
-async function loadData() {
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get('id');
-    // console.log(id);
-    const member = await fetchMember(id);
-    //     console.log(member);
+// async function loadData() {
+//     const params = new URLSearchParams(window.location.search);
+//     const id = params.get('id');
+//     // console.log(id);
+//     const member = await fetchMember(id);
+//     //     console.log(member);
 
-    const name = document.getElementById('name');
-    name.id = member.name;
-    const age = document.getElementById('age');
-    const funFact = document.getElementById('fun-fact');
-    const hobbies = document.getElementById('hobbies');
-    const dislikes = document.getElementById('dislikes');
-    const favFood = document.getElementById('fav-food');
-    const image = document.getElementById('member-image');
+//     const name = document.getElementById('name');
+//     name.id = member.name;
+//     const age = document.getElementById('age');
+//     const funFact = document.getElementById('fun-fact');
+//     const hobbies = document.getElementById('hobbies');
+//     const dislikes = document.getElementById('dislikes');
+//     const favFood = document.getElementById('fav-food');
+//     const image = document.getElementById('member-image');
 
-    name.textContent = member.name;
+//     name.textContent = member.name;
 
-    age.textContent = `Age: ${member.age}`;
-    funFact.textContent = `Fun Fact: ${member.fun_fact}`;
-    hobbies.textContent = `Hobbies: ${member.hobbies}`;
-    dislikes.textContent = `Dislikes: ${member.dislikes}`;
-    favFood.textContent = `Favorite Food: ${member.fav_food}`;
-    image.src = `../assets/${member.name}.jpeg`;
+//     age.textContent = `Age: ${member.age}`;
+//     funFact.textContent = `Fun Fact: ${member.fun_fact}`;
+//     hobbies.textContent = `Hobbies: ${member.hobbies}`;
+//     dislikes.textContent = `Dislikes: ${member.dislikes}`;
+//     favFood.textContent = `Favorite Food: ${member.fav_food}`;
+//     image.src = `../assets/${member.name}.jpeg`;
 
-    // const div = fetchMember(member);
-    // memberDetailContainer.append(div);
-    // console.log(member);
-}
-loadData();
+// const div = fetchMember(member);
+// memberDetailContainer.append(div);
+// console.log(member);
+// }
+// loadData();
 
 // eslint-disable-next-line space-before-function-paren
 // window.addEventListener('load', async () => {
@@ -47,11 +47,12 @@ loadData();
 // // });
 
 // eslint-disable-next-line space-before-function-paren
-// window.addEventListener('load', async () => {
-//     const member = await fetchMember();
-//     const li = renderMemberDetails();
-//     console.log(member);
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const member = await fetchMember(id);
+    const memberDetails = renderMemberDetails(member);
+    // console.log(member);
 
-//     memberList.append(li);
-
-// });
+    memberList.append(memberDetails);
+});
--- render-utils.js
+++ render-utils.js
@@ -2,34 +2,36 @@ export function renderDetails(member) {
     const li = document.createElement('li');
     li.classList.add('member-list');
     const a = document.createElement('a');
+    const image = document.createElement('img');
     a.href = `/details/?id=${member.id}`;
+    image.src = `../assets/${member.name}.jpeg`;
     a.textContent = member.name;
-    li.append(a);
+    li.append(a, image);
     // console.log(a);
     return li;
 }
 
-// export function renderMemberDetails() {
-//     const memberDetailContainer = document.createElement('member-detail-container');
-//     const member = document.createElement('div');
-//     const name = document.createElement('name');
-//     name.id = member.name;
-//     const age = document.createElement('age');
-//     const funFact = document.createElement('fun-fact');
-//     const hobbies = document.createElement('hobbies');
-//     const dislikes = document.createElement('dislikes');
-//     const favFood = document.createElement('fav-food');
-//     const image = document.createElement('member-image');
+export function renderMemberDetails(person) {
+    // const div = document.createElement('div');
+    const member = document.createElement('div');
+    const name = document.createElement('p');
+    name.id = member.name;
+    const age = document.createElement('p');
+    const funFact = document.createElement('p');
+    const hobbies = document.createElement('p');
+    const dislikes = document.createElement('p');
+    const favFood = document.createElement('p');
+    const image = document.createElement('img');
 
-//     name.textContent = member.name;
+    name.textContent = person.name;
 
-//     age.textContent = `Age: ${member.age}`;
-//     funFact.textContent = `Fun Fact: ${member.fun_fact}`;
-//     hobbies.textContent = `Hobbies: ${member.hobbies}`;
-//     dislikes.textContent = `Dislikes: ${member.dislikes}`;
-//     favFood.textContent = `Favorite Food: ${member.fav_food}`;
-//     image.src = `../assets/${member.name}.jpeg`;
+    age.textContent = `Age: ${person.age}`;
+    funFact.textContent = `Fun Fact: ${person.fun_fact}`;
+    hobbies.textContent = `Hobbies: ${person.hobbies}`;
+    dislikes.textContent = `Dislikes: ${person.dislikes}`;
+    favFood.textContent = `Favorite Food: ${person.fav_food}`;
+    image.src = `../assets/${person.name}.jpeg`;
 
-//     memberDetailContainer.append(name, age, funFact, hobbies, dislikes, favFood, image);
-//     return member;
-// }
+    member.append(name, age, funFact, hobbies, dislikes, favFood, image);
+    return member;
+}
